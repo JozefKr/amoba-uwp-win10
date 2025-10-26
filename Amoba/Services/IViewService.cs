e8d4f5d5b27e97b7fe7db1c4344f52ff09386eeb
JozefKr <krutilla.jozsef@gmail.com>
2025-10-26 17:56:23 +0100

    gitignore és hiányzó fájl feltöltése

---
+++ Amoba/Services/IViewService.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using GalaSoft.MvvmLight;
+using System;
+
+namespace Amoba.Services
+{
+    // A ViewService által hivatkozott interfész frissítése
+    public interface IViewService
+    {
+        void RegisterPage(Type vm, Type page);
+
+        void OpenPage<T>(params NamedParameter[] parameters) where T : ViewModelBase;
+
+        void OpenDialog<T>(params NamedParameter[] parameters) where T : ViewModelBase;
+    }
+}
