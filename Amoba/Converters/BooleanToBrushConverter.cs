3d892ab39859d9533d9d1aefbc8fe3b7adf1de03
JozefKr <krutilla.jozsef@gmail.com>
2025-11-01 22:52:33 +0100

    Chat Buborékok Színezése

---
+++ Amoba/Converters/BooleanToBrushConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Amoba.Converters
+{
+    public class BooleanToBrushConverter : IValueConverter
+    {
+        // Ezeket az erőforrásokat a XAML-ben definiáljuk
+        public SolidColorBrush TrueBrush { get; set; }
+        public SolidColorBrush FalseBrush { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool isMine && isMine) ? TrueBrush : FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
